d985825185b26d4146299104b77d4ee85664123b
keerthanagowda206-creator <keerthanagowda206@gmail.com>
2026-06-28 17:39:01 +0530

    Update cleaning.py

--- Data_Cleaning_Project/cleaning.py
+++ Data_Cleaning_Project/cleaning.py
@@ -1,28 +1,24 @@
+import os
 import pandas as pd
 
-
 def load_and_clean_data():
-    # Load dataset (update file name if needed)
-    df = pd.read_csv("raw_data/Sample - Superstore.csv", encoding="latin1")
 
-    # ---------------------------
-    # CLEANING
-    # ---------------------------
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, "raw_data", "Sample - Superstore.csv")
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found at: {file_path}")
+
+    df = pd.read_csv(file_path, encoding="latin1")
+
     df.drop_duplicates(inplace=True)
     df.dropna(inplace=True)
 
-    # Standardize column names
     df.columns = df.columns.str.lower()
 
-    # Convert date column
     df["order date"] = pd.to_datetime(df["order date"], errors="coerce")
-
-    # Remove rows with invalid dates
     df = df.dropna(subset=["order date"])
 
-    # ---------------------------
-    # FEATURE ENGINEERING (INDUSTRY LEVEL)
-    # ---------------------------
     df["year"] = df["order date"].dt.year
     df["month"] = df["order date"].dt.to_period("M").astype(str)
     df["quarter"] = df["order date"].dt.quarter
